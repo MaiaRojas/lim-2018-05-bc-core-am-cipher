9bd99d6f84662ef48ba480d6b4965a998e0a1a5b
yulissateran <yulissa.lteran@gmail.com>
2018-05-24 08:53:24 -0500

    agregando formula y código de descifrado

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 //codigo dd cifrado
+var option =parseInt(prompt('¿Que deseas hacer?\n 1.Cifrar\n 2.Descifrar'));
 var text = prompt('Ingresa tu texto o cadena').toUpperCase();
 var displacement  = parseInt(prompt('Indica cuantos espacios van a desplazarse'));
 var sizetext = text.length;
 alert(sizetext);
+var index = 0;
+var letter = " ";
+var ofAscii='';
+var formula= '';
+
+
 var cipher = function(){
-  var index = 0;
-  var letter = " ";
   for ( index; index <sizetext ; index ++) {
       if (text[index]!=" ") {
-        var formula = (text.charCodeAt(index)-65 + displacement)% 26 + 65;
-        var ofAscii = String.fromCharCode(formula);
+         formula = (text.charCodeAt(index)-65 + displacement)% 26 + 65;
+         ofAscii = String.fromCharCode(formula);
         letter= letter+ ofAscii;
      }else {
         letter = letter + " ";
@@ -19,10 +24,26 @@ var cipher = function(){
   return(letter);
 }
 
-alert (cipher());
 
+var decipher = function(){
+  for ( index; index <sizetext ; index ++) {
+      if (text[index]!=" ") {
+         formula = 90-(90 - text.charCodeAt(index)+ displacement)% 26;
+         ofAscii = String.fromCharCode(formula);
+        letter= letter+ ofAscii;
+     }else {
+        letter = letter + " ";
+     }
+ }
 
+  return(letter);
+}
 
+if (option === 1){
+  alert (cipher());
+ }else if(option === 2){
+   alert (decipher());
+ }
 
 //(x+n)%26 formula
 /*(0+33)%26
