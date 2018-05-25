7a08a6408ddd01c3f58f88f0aca2cf0632bd0716
yulissateran <yulissa.lteran@gmail.com>
2018-05-25 09:17:58 -0500

    subiendo cambios listos para el gh-pages del demo1

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,47 @@
+
 window.cipher = {
-  // ... 
+  code:code,
+  decipher:decode
 };
+
+ function code (text,displacement) {
+    let i = 0;
+    let ofAscii='';
+    let formula= '';
+    let ascii= '';
+    text=text.toUpperCase();
+    let letter='';
+    for ( i; i <text.length; i++) {
+        if (text[i]!=" ") {
+          ascii=text.charCodeAt(i);
+          formula = ((ascii-65 + displacement)% 26) + 65;
+           ofAscii = String.fromCharCode(formula);
+         letter= letter+ ofAscii;
+       }else {
+          letter = letter + " ";
+        }
+   }
+   return (letter);
+  };
+
+  function decode (text,displacement) {
+    let i = 0;
+    let ofAscii='';
+    let formula= '';
+    let ascii='';
+    text=text.toUpperCase();
+    let letter='';
+    for ( i; i <text.length ; i++) {
+        if (text[i]!=" ") {
+           formula = 90-(90 - text.charCodeAt(i)+ displacement)% 26;
+           ofAscii = String.fromCharCode(formula);
+          letter= letter+ ofAscii;
+       }else {
+          letter = letter + " ";
+       }
+        return (letter);
+   }
+
+    }
+
+ 
--- src/index.js
+++ src/index.js
@@ -1,132 +1,20 @@
 
-window.cipher = {
-  Cipher:(text,displacement)=>{
-    let i = 0;
-    const ofAscii='';
-    const formula= '';
-    text=text.toUpperCase();
-
-    for ( i; i <text.length; i++) {
-        if (text[i]!=" ") {
-          ascii=text.charCodeAt(i);
-          formula = ((ascii-65 + displacement)% 26) + 65;
-           ofAscii = String.fromCharCode(formula);
-         letter= letter+ ofAscii;
-       }else {
-          letter = letter + " ";
-        }
-       return (letter);
-   }
-  },
-  Decipher:(text,displacement)=> {
-    let i = 0;
-    const ofAscii='';
-    const formula= '';
-    text=text.toUpperCase();
-    for ( i; i <text.length ; i++) {
-        if (text[i]!=" ") {
-           formula = 90-(90 - text.charCodeAt(i)+ displacement)% 26;
-           ofAscii = String.fromCharCode(formula);
-          letter= letter+ ofAscii;
-       }else {
-          letter = letter + " ";
-       }
-        return (letter);
-   }
-
-    }
-
-  };
-
-
-
-
-
-  
-
-
-
-
-///
-
-document.getElementById('optionCifrar').addEventListener('click',function(){
+document.getElementById('cifrar').addEventListener('click',function(){
     let displacement =document.getElementById('displacement').value;
+    displacement=parseInt(displacement);
     let text =document.getElementById("text").value;
-  const letter=cipher.Cipher(text,displacement);
+    let word=cipher.code(text,displacement);
   let result =document.getElementById('result');
-     result.innerHTML=letter;
+     result.innerHTML=word;
 });
-document.getElementById('optionDescifrar').addEventListener('click',function(){
+
+
+document.getElementById('descifrar').addEventListener('click',function(){
   let displacement =document.getElementById('displacement').value;
+  displacement=parseInt(displacement);
   let text=document.getElementById("text").value;
-  const letter=cipher.Decipher(text,displacement);
+  let word2=cipher.decipher(text,displacement);
 let result =document.getElementById('result');
-  result.innerHTML=letter;
+  result.innerHTML=word2;
 });
 
-
-
-
-
-
-
-
-
-
-
-
-var option =parseInt(document.getElementById("option"));
-var text = document.getElementById("text").toUpperCase();
-var displacement  = parseInt(prompt('Indica cuantos espacios van a desplazarse'));
-var sizetext = text.length;
-alert(sizetext);
-var index = 0;
-var letter ="";
-var ofAscii='';
-var formula= '';
-
-
-var cipher = function(){
-  for ( index; index <sizetext ; index ++) {
-      if (text[index]!=" ") {
-         formula = (text.charCodeAt(index)-65 + displacement)% 26 + 65;
-         ofAscii = String.fromCharCode(formula);
-        letter= letter+ ofAscii;
-     }else {
-        letter = letter + " ";
-     }
- }
-
-  return(letter);
-}
-
-
-var decipher = function(){
-  for ( index; index <sizetext ; index ++) {
-      if (text[index]!=" ") {
-         formula = 90-(90 - text.charCodeAt(index)+ displacement)% 26;
-         ofAscii = String.fromCharCode(formula);
-        letter= letter+ ofAscii;
-     }else {
-        letter = letter + " ";
-     }
- }
-
-  return(letter);
-}
-
-if (option === 1){
-  alert (cipher());
- }else if(option === 2){
-   alert (decipher());
- }
-
-//(x+n)%26 formula
-/*(0+33)%26
-33%26
-7
-
-codigo ASCII
-(x-65+       n)       % 26            + 65
-ASCII/ VALOR FIJO/RESIDUO DE 26/PARA OBTENER CÓDIGO ASCII
-*/
