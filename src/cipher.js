5e65f754f4038ae65a8b3df7079029325bcfbd38
yulissateran <yulissa.lteran@gmail.com>
2018-05-27 23:19:27 -0500

    Agregando estilos y metodo createCipherWithOfset

--- src/cipher.js
+++ src/cipher.js
@@ -1,30 +1,25 @@
 
 window.cipher = {
-  code:code,
-  decipher:decode
-};
-
- function code (text,displacement) {
-    let i = 0;
-    let ofAscii='';
-    let formula= '';
-    let ascii= '';
-    text=text.toUpperCase();
-    let letter='';
-    for ( i; i <text.length; i++) {
-        if (text[i]!=" ") {
-          ascii=text.charCodeAt(i);
-          formula = ((ascii-65 + displacement)% 26) + 65;
-           ofAscii = String.fromCharCode(formula);
-         letter= letter+ ofAscii;
-       }else {
-          letter = letter + " ";
-        }
-   }
-   return (letter);
-  };
-
-  function decode (text,displacement) {
+  encode:(text,offset)=> {
+      let i = 0;
+      let ofAscii='';
+      let formula= '';
+      let ascii= '';
+      text=text.toUpperCase();
+      let letter='';
+      for ( i; i <text.length; i++) {
+          if (text[i]!=" ") {
+            ascii=text.charCodeAt(i);
+            formula = ((ascii-65 + offset)% 26) + 65;
+             ofAscii = String.fromCharCode(formula);
+           letter= letter+ ofAscii;
+         }else {
+            letter =" ";
+          }
+     }
+     return (letter);
+    },
+  decode:(text,offset)=>{
     let i = 0;
     let ofAscii='';
     let formula= '';
@@ -34,12 +29,19 @@ window.cipher = {
     for ( i; i <text.length ; i++) {
         if (text[i]!=" ") {
           ascii=text.charCodeAt(i);
-           formula = 90-(90 -ascii+ displacement)% 26;
+           formula = 90-(90 -ascii+ offset)% 26;
            ofAscii = String.fromCharCode(formula);
           letter= letter+ ofAscii;
        }else {
-          letter = letter + " ";
+          letter =" ";
        }
    }
    return (letter);
+    },
+  createCipherWithOffset : (offset)=>{
+    return window.cipher0 = {
+      encode0 :(text)=>{return cipher.encode(text,offset)},
+      decode0 :(text)=>{return cipher.decode(text,offset)}
     };
+  }
+  };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-
 document.getElementById('cifrar').addEventListener('click',function(){
-    let displacement =document.getElementById('displacement').value;
-    displacement=parseInt(displacement);
-    let text =document.getElementById("text").value;
-    let word=cipher.code(text,displacement);
-  let result =document.getElementById('result');
-     result.innerHTML=word;
+  let offset =document.getElementById('offset').value;
+  offset=parseInt(offset);
+  let text =document.getElementById("text").value;
+  let word=(cipher.createCipherWithOffset(offset)).encode0(text);
+let result =document.getElementById('result');
+   result.innerHTML=word;
 });
 
 
 document.getElementById('descifrar').addEventListener('click',function(){
-  let displacement =document.getElementById('displacement').value;
-  displacement=parseInt(displacement);
-  let text=document.getElementById("text").value;
-  let word2=cipher.decipher(text,displacement);
+let offset =document.getElementById('offset').value;
+offset=parseInt(offset);
+let text=document.getElementById("text").value;
+let word2=(cipher.createCipherWithOffset(offset)).decode0(text);
 let result =document.getElementById('result');
-  result.innerHTML=word2;
+result.innerHTML=word2;
 });
 
